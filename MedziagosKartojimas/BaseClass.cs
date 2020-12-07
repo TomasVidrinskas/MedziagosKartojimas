9cdc3b9166df27887fb115670ff61ea20f7037f9
Tomas V <tomcesz@gmail.com>
2020-12-07 20:44:09 +0200

    Add project files.

---
+++ MedziagosKartojimas/BaseClass.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MedziagosKartojimas
+{
+    public abstract class BaseClass
+    {
+        public BaseClass(string name)
+        {
+            Name = name;
+        }
+
+        public string Name { get; set; }
+
+        public NameMetodas()
+        {
+
+        }
+    }
+
+}
